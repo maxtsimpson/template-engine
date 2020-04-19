73a37a9622f96dfa243320242a5c4f6fbef2bc34
Max Simpson <maxsimpson95@gmail.com>
2020-04-19 14:13:32 +0800

    some work on employee const and validation of input

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,36 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name,id,email) {
+        
+        this.name = name;
+        //would be good if this was unique and auto-generated
+        this.id = id;
+        this.email = email;
+    }
+
+    //validate each property
+    isValidId(id) {
+        if(typeof(id) !== "number"){
+            throw "The employee name should be a number";
+        }
+    }
+
+    isValidType(propertyName,input,type){
+        if(typeof(input) !== type){
+            throw `the ${propertyName} should be a ${type}`
+        }
+        return true;
+    }
+
+    isValidName(name) {
+        if(typeof(name) !== "string"){
+            throw "The employee name should be a string";
+        }
+    }
+
+    isValidName(name) {
+        if(typeof(name) !== "string"){
+            throw "The employee name should be a string";
+        }
+    }
+}
