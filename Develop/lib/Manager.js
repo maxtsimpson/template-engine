a78fc8aebc5ceae118a9a1ade360839a20ca971f
Max Simpson <maxsimpson95@gmail.com>
2020-04-23 21:53:40 +0800

    plenty of changes incl sick ascii banner

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,12 +1,16 @@
 const Employee = require("../lib/Employee");
 
 class Manager extends Employee{
+    
+    officeNumber;
+
     constructor(name,id,email,officeNumber) {
         super(name,id,email)
         if (this.isValidType("officeNumber",officeNumber,"number")) {
             this.officeNumber = officeNumber;
         }
-    }
+    }  
+    
     getOfficeNumber(){
         return this.officeNumber;
     }
--- Develop/test/Manager.test.js
+++ Develop/test/Manager.test.js
@@ -1,10 +1,9 @@
 const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(e.officeNumber()).toBe(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
@@ -16,12 +15,12 @@ test('getRole() should return "Manager"', () => {
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.officeNumber()).toBe(testValue);
 });
 
 test("Can set officeNumber via setOfficeNumber method", () => {
   const e = new Manager("Foo", 1, "test@test.com", 150);
   const officeNumber = 200;
-  e.setOfficeNumber(officeNumber);
-  expect(e.officeNumber).toBe(officeNumber);
+  e.officeNumber(officeNumber);
+  expect(e.officeNumber()).toBe(officeNumber());
 });
